dbac1a08cad2938cc972a15f76a82430d9ad93e7
FarukIb <67324859+FarukIb@users.noreply.github.com>
2021-07-23 23:28:56 +0200

    Update gfssoc3s3_PalinDrone.cpp

--- DMOJ/Miscellaneous/gfssoc3s3_PalinDrone.cpp
+++ DMOJ/Miscellaneous/gfssoc3s3_PalinDrone.cpp
@@ -1,10 +1,10 @@
-// the formula for the number of palindromic numbers with length n is
+// the formula for the number of palindromic numbers of length n is
 // (9 * pow(10, (n - 1) / 2) ) -- cool article https://www.geeksforgeeks.org/count-of-n-digit-palindrome-numbers/
 // using this formula we can sum up the output of the formula for every number up to (n + 1) to get the answer
-// this by itself isn't enough as n can be very large
+// this by itself is enough to get 80% of the points
 
 // notice that when n is larger than 18, the formula will output a number divisible by 1000000000(because of pow(10, (n - 1) / 2))
-// so for every n larger than 18 the answer is the same, since the formula will 
+// so for every n larger than 18, the answer is the same since the output of the formula mod 1000000000 will always be equal to 0
 
 #include <iostream>
 #include <algorithm>
